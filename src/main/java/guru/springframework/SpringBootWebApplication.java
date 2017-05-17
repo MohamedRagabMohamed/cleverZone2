dd15f57fea0ade5ed085ad3a8436b1b02fc02549
nour <supernour09@gmail.com>
2017-05-17 22:40:17 +0200

    Deply23

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -3,7 +3,10 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +15,12 @@ import org.springframework.stereotype.Component;
  * The Class SpringBootWebApplication.
  */
 @Component("guru.springframework")
+@Configuration
 @ComponentScan
 @EnableJpaRepositories
 @EnableAutoConfiguration
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer {
     
     /**
      * The main method.
@@ -25,5 +29,9 @@ public class SpringBootWebApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+	 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
     }
 }
